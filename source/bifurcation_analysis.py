7b5fc014b1381a208af5b9ecc6bfc158c66eddfb
jullcifer <julls.mikhailova@gmail.com>
2025-08-15 17:17:04 +0200

    saving everything in the parent directory

--- source/bifurcation_analysis.py
+++ source/bifurcation_analysis.py
@@ -1,8 +1,9 @@
+import os
 import numpy as np
 from data_processing import classify_and_read_data
 from bifurcation_plot import plot_bifurcation_diagram
 
-def bifurcationdiagram(base_directory, saveplot=True):
+def bifurcationdiagram(parent_directory, saveplot=True):
     """
     This function uses the results from the classification to create the 
     bifurcation diagram. It uses the uncleaned results, i.e. with the 
@@ -14,7 +15,7 @@ def bifurcationdiagram(base_directory, saveplot=True):
         base_directory (str): Directory where the data is stored.
         saveplot (bool): Whether to save the plot. Defaults to True.
     """
-    simulation_data = classify_and_read_data(base_directory)
+    simulation_data = classify_and_read_data(parent_directory)
     aS_values, nuvalues, categoryvalues = [], [], []
     for nu, aS_data in simulation_data.items():
         for aS, details in aS_data.items():
@@ -30,6 +31,6 @@ def bifurcationdiagram(base_directory, saveplot=True):
         i = np.where(nu_unique == nu)[0][0]
         j = np.where(aS_unique == aS)[0][0]
         category_matrix[i, j] = category
-    np.savetxt("category_matrix.txt", category_matrix, fmt='%.0f', delimiter=', ')
+    np.savetxt(os.path.join(parent_directory, "category_matrix.txt"), category_matrix, fmt='%.0f', delimiter=', ')
     if saveplot:
-        plot_bifurcation_diagram(aS_unique, nu_unique, category_matrix, f"{base_directory}/BifurcationGridPlot.png")
+        plot_bifurcation_diagram(aS_unique, nu_unique, category_matrix, f"{parent_directory}/BifurcationGridPlot.png")
